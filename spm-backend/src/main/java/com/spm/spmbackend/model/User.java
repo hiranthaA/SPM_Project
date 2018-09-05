e5cf4acbf271aa38500a416ac0aa41e441be90c3
shavi94 <30893096+shavi94@users.noreply.github.com>
2018-09-05 10:06:27 +0530

    edit mode

--- spm-backend/src/main/java/com/spm/spmbackend/model/User.java
+++ spm-backend/src/main/java/com/spm/spmbackend/model/User.java
@@ -1,5 +1,11 @@
 package com.spm.spmbackend.model;
 
 public class User {
-
+  String companyName;
+  String address;
+  int telephone;
+  int fax;
+  String email;
+  String password;
+  int noOfInterns;
 }
