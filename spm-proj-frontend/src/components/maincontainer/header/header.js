3e39c0477a1fb34c7c3544a131ce482087d01bba
GeethSameera <geethsameera077@gmail.com>
2018-09-08 13:30:36 +0530

    Crash recovery

---
+++ spm-proj-frontend/src/components/maincontainer/header/header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render(){
+    return(
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="#">Industrial Training Dashboard</a>z
+      </nav>
+    );
+  }
+}
+
+export default Header;
