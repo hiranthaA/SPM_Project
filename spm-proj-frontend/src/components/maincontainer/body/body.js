b36ea3df0a3914387b6d4e32ef641e35a70c6761
Shavinda <shavindaherath@gmail.com>
2018-09-08 18:33:18 +0530

    mas new new

---
+++ spm-proj-frontend/src/components/maincontainer/body/body.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import Login from './login/login';
+import CompanyDash from './dashboards/companydash/comapnydash';
+import AdminDash from './dashboards/admindash/admindash';
+import StudentDash from './dashboards/studentdash/studentdash';
+import Registration from './registration/registration';
+import SupervisorDash from './dashboards/supervisordash/supervisordash';
+
+
+class Body extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.setLoadedBody = this.setLoadedBody.bind(this);
+
+    this.state={
+      loaded_body : "supervisor"
+    }
+  }
+
+  setLoadedBody(x){
+    this.setState({loaded_body:x});
+  }
+
+  render(){
+
+    let body;
+
+    if(this.state.loaded_body==="login"){
+      body=(
+        < Login setview={this.setLoadedBody}/>
+      );
+    }
+    else if(this.state.loaded_body==="company"){
+      body=(
+        < CompanyDash/>
+      );
+    }
+    else if(this.state.loaded_body==="admin"){
+      body=(
+        < AdminDash/>
+      );
+    }
+    else if(this.state.loaded_body==="student"){
+      body=(
+        < StudentDash/>
+      );
+    }
+    else if(this.state.loaded_body==="registration"){
+      body=(
+        < Registration/>
+      );
+    }
+    else if(this.state.loaded_body==="supervisor"){
+      body=(
+        < SupervisorDash/>
+      );
+    }
+
+    return(
+      <div>
+          {body}
+      </div>
+    );
+  }
+}
+
+export default Body;
