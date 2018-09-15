b7d66902a4811d2c80b3bb845b2b92521a2550f8
hiranthaA <30893182+hiranthaA@users.noreply.github.com>
2018-09-16 00:01:29 +0530

    logout functionality added

--- spm-proj-frontend/src/components/maincontainer/header/header.js
+++ spm-proj-frontend/src/components/maincontainer/header/header.js
@@ -1,11 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import "./header.css";
 
 class Header extends Component {
-  render(){
-    return(
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <a class="navbar-brand" href="#">Industrial Training Dashboard</a>
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-between">
+        <a class="navbar-brand" href="#" id="headertext">Industrial Training Dashboard</a>
+        <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#exampleModalCenter">Logout</button>
+
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Logout</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p>Are you sure you want to logout?</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal" onClick={this.props.logout}>Logout</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
       </nav>
+
+
     );
   }
 }
--- spm-proj-frontend/src/components/maincontainer/maincontainer.js
+++ spm-proj-frontend/src/components/maincontainer/maincontainer.js
@@ -3,12 +3,29 @@ import Header from './header/header';
 import Body from './body/body';
 
 class MainContainer extends Component {
+
+  constructor(props){
+    super(props);
+    this.childBody = React.createRef();
+    this.setLoggedStatus=this.setLoggedStatus.bind(this);
+    this.state={
+      loggedStatus : false
+    }
+  }
+
+  setLoggedStatus(){
+    this.setState({loggedStatus : !this.state.loggedStatus});
+    console.log("main container loggedstatus method reached");
+    console.log(this.state.loggedStatus);
+    this.childBody.current.setLoadedBodyToLogin();
+  }
+
   render(){
 
     return(
       <div>
-        <Header />
-        <Body />
+        <Header logout={this.setLoggedStatus}/>
+        <Body ref={this.childBody}/>
       </div>
     );
   }
